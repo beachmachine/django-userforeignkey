ad21f17bb90d3d40b37b02990d108aae85b4999a
Christian Kreuzberger <ckreuzberger@anexia-it.com>
2016-09-27 10:10:28 +0200

    Fix middleware for django 1.8 and 1.9

--- django_userforeignkey/middleware.py
+++ django_userforeignkey/middleware.py
@@ -1,8 +1,26 @@
 # -*- coding: utf-8 -*-
 import logging
-
-from django.utils.deprecation import MiddlewareMixin
 from django_userforeignkey.request import set_current_request
+
+# import Django 1.10 middleware
+try:
+    from django.utils.deprecation import MiddlewareMixin
+except:
+    class MiddlewareMixin(object):
+        def __init__(self, get_response=None):
+            self.get_response = get_response
+            super(MiddlewareMixin, self).__init__()
+    
+        def __call__(self, request):
+            response = None
+            if hasattr(self, 'process_request'):
+                response = self.process_request(request)
+            if not response:
+                response = self.get_response(request)
+            if hasattr(self, 'process_response'):
+                response = self.process_response(request, response)
+            return response
+
 
 logger = logging.getLogger(__name__)
 
@@ -15,9 +33,6 @@ class UserForeignKeyMiddleware(MiddlewareMixin):
     request everywhere, and don't need to pass it to every
     function.
     """
-
-    def __init__(self, get_response=None):
-        self.get_response = get_response
 
     def process_request(self, request):
         logger.debug(u"Process request")
